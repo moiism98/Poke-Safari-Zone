db2b6821426d4d1ecfff90d1e5c0f3587f6aeabf
moiism98 <moisessantana.tee.2014@gmail.com>
2024-02-28 16:44:06 +0000

    wip(player): level up notification and unlocks

--- Poke-Safari/src/pages/player/PokemonList.tsx
+++ Poke-Safari/src/pages/player/PokemonList.tsx
@@ -3,7 +3,7 @@ import useApp from "src/components/App/hook/useApp"
 import { useContext } from "react"
 import { Context } from "src/context/AppContext"
 import { Image } from "react-bootstrap"
-import { Popover } from "antd"
+import { Popover, notification } from "antd"
 
 const PokemonList = () => {
 
@@ -11,6 +11,21 @@ const PokemonList = () => {
 
     const  {FirstLetterToUpper } = useApp();
 
+    const onNotification = () => {
+    
+        notification.open({
+            message: <h5>You have reached level {saveFile?.player?.level}</h5>,
+            description: 
+            <div style={{ display: "flex", alignItems: 'center', justifyContent: 'space-evenly'}}>
+                <Image src={ saveFile?.options?.icon?.icon }/>
+                <div>DESCRIPTION</div>
+            </div>,
+            duration: 3,
+            closeIcon: false,
+            placement: 'top'
+        })
+    
+    }
 
     return(
         <GameScreen>
@@ -20,6 +35,7 @@ const PokemonList = () => {
 
                         saveFile.myPokemons.map(pokemon => (
                             <Popover
+                                key={ pokemon.id }
                                 content={(
                                     <div style={{display: 'flex', flexDirection: 'column'}}>
                                         <span style={ pokemon.shiny ? {color: '#f9be19'} : {color: 'lightskyblue'} }><strong>{ FirstLetterToUpper(pokemon.name) }</strong></span>
@@ -27,6 +43,7 @@ const PokemonList = () => {
                                         <span>Weight: <strong>{ pokemon.weight } kg.</strong></span>
                                         <span>Moves: <strong>{ pokemon.moves.map(move => FirstLetterToUpper(move.move.name)).join(", ") }</strong></span>
                                         { pokemon.held_item ? <span>Held item: <strong>{ FirstLetterToUpper(pokemon.held_item.item.name) }</strong></span> : null}
+                                        { pokemon.ability ? <span>Ability: <strong>{ FirstLetterToUpper(pokemon.ability.ability.name) }</strong></span> : null}
                                     </div>    
                                 )}
                             >
@@ -35,6 +52,7 @@ const PokemonList = () => {
                                     src={pokemon.shiny ? pokemon.sprites.front_shiny : pokemon.sprites.front_default}
                                     width={125}
                                     height={125}
+                                    onClick={() => onNotification()}
                                 />
                             </Popover>
                         ))
