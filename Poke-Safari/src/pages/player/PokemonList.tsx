edf6e786876d23ec58015f20f3a427b4cc00c8b0
moiism98 <moisessantana.tee.2014@gmail.com>
2024-03-08 21:07:58 +0000

    fix: add/remove pokemon from team, when pokemon released, drop it also from the team

--- Poke-Safari/src/pages/player/PokemonList.tsx
+++ Poke-Safari/src/pages/player/PokemonList.tsx
@@ -1,23 +1,45 @@
 import GameScreen from "src/components/GameScreen/GameScreen";
-import { useContext } from "react";
-import { Context } from "src/context/AppContext"
-import { Button, Image } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import useApp from "src/components/App/hook/useApp";
+import './PokemonList.css';
+import { useContext, useState} from "react";
+import { Context } from "src/context/AppContext"
+import { Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FloatButton, Modal } from "antd";
+import { DeleteFilled } from '@ant-design/icons';
+import { CatchedPokemon } from "src/interfaces/interfaces";
 
 const PokemonList = () => {
 
-    const { saveFile, SaveGame, setPokemonDetails } = useContext(Context);
+    const { saveFile, pokemonTeam, setPokemonTeam , setPokemonDetails, SaveGame } = useContext(Context);
 
-    const { FirstLetterToUpper } = useApp()
+    const { FirstLetterToUpper } = useApp();
 
-    /*const onConfirm = () => {
+    const [ releasePokemon, setReleasePokemon ] = useState<CatchedPokemon>();
+
+    const [ release, setRelease ] = useState<boolean>(false);
+
+    const [ openModal, setOpenModal ] = useState<boolean>(false);
+
+    const onRelease = (releasePokemon: CatchedPokemon | undefined) => {
 
         const saveFileCopy = saveFile;
-
+        
         if(saveFileCopy)
         {
+            const teamPokemon = pokemonTeam.find(pokemon => pokemon.listId == releasePokemon?.listId);
+
+            if(teamPokemon) // if we found the released pokemon on the team, we remove this pokemon from the team!
+            {
+                // remake the pokemon team discarding the ALL released pokemon! (inside myPokemon discard the pokemon already realeased and the current one released)
+
+               saveFileCopy.pokemonTeam = saveFileCopy?.myPokemons.filter(pokemon => pokemon.listId != teamPokemon.listId && !pokemon.released); 
+
+               setPokemonTeam(saveFileCopy.pokemonTeam);
+            }
+
             const pokemon = saveFileCopy.myPokemons.find(pokemon => pokemon == releasePokemon)
+
             if(pokemon)
             {
                 pokemon.released = true;
@@ -26,49 +48,96 @@ const PokemonList = () => {
             }
         }
         
-        setConfirm(false);
-    }*/
+        setOpenModal(false);
+    }
+
 
     return(
-        <GameScreen>
-            <div style={{ height: '100%', width:'100%', overflowY: 'auto'}}>
-                {
-                    saveFile ?
+        <>
+            <GameScreen>
+                <div className="pokemonList">
+                    {
+                        saveFile ?
 
-                        saveFile.myPokemons.map(pokemon => (
+                            saveFile.myPokemons.map(pokemon => (
+    
+                                !pokemon.released ?
 
-                            !pokemon.released ?
+                                    !release ?
 
-                            <Link key={ pokemon.listId } onClick={() => setPokemonDetails(pokemon) } to={`${pokemon.name}`}> 
-                                <Image  
-                                    title={ pokemon.nickname ? pokemon.nickname : FirstLetterToUpper(pokemon.name) }
-                                    src={ pokemon.shiny ? pokemon.sprites.front_shiny : pokemon.sprites.front_default }
-                                    width={125}
-                                    height={125}
-                                />
-                            </Link> : null
-                        ))
+                                        <Link key={ pokemon.listId } onClick={() => setPokemonDetails(pokemon) } to={`${pokemon.name}`}> 
+                                            <Image  
+                                                title={ pokemon.nickname ? pokemon.nickname : FirstLetterToUpper(pokemon.name) }
+                                                src={ pokemon.shiny ? pokemon.sprites.front_shiny : pokemon.sprites.front_default }
+                                            />
+                                        </Link> 
 
-                    : null
-                }
-            </div>
-            <Button
-                onClick={() => {
-                    saveFile?.myPokemons.map(pokemon => {
-                    
-                        if(pokemon.released)
-                        {
-                            pokemon.released = false;
-                        }
-                    })
+                                        :
+                                        <>
+                                            <Image 
+                                                className='releasePokemon'
+                                                key={ pokemon.listId }
+                                                title={ pokemon.nickname ? pokemon.nickname : FirstLetterToUpper(pokemon.name) }
+                                                src={ pokemon.shiny ? pokemon.sprites.front_shiny : pokemon.sprites.front_default }
+                                                onClick={() => {
 
-                    const saveFileCopy = saveFile;
-                    
-                    if(saveFileCopy)
-                        SaveGame(saveFileCopy);
+                                                    setOpenModal(true);
+
+                                                    setReleasePokemon(pokemon);
+                                                }}
+                                            />
+                                        </>
+                                
+                                : null
+                            ))
+
+                        : null
+
+                    }
+                </div>
+
+                {/*<Button
+                    onClick={() => {
+                        saveFile?.myPokemons.map(pokemon => {
+                        
+                            if(pokemon.released)
+                            {
+                                pokemon.released = false;
+                            }
+                        })
+
+                        const saveFileCopy = saveFile;
+                        
+                        if(saveFileCopy)
+                            SaveGame(saveFileCopy);
+                    }}
+                >
+                    Take back pokes
+                </Button>*/}
+
+            </GameScreen>
+
+            <FloatButton  
+                className="releaseButton"
+                tooltip={ release ? <div>Click to exit from release pokemon mode!</div> : <div>Click to enter on release pokemon mode!</div>}
+                icon= { <DeleteFilled /> } 
+                onClick={() => setRelease(oldValue => !oldValue)}
+            />
+
+            <Modal
+                open={ openModal }
+                closeIcon={ false }
+                title={`Are you sure you want to release ${ releasePokemon?.nickname ? releasePokemon.nickname : FirstLetterToUpper(releasePokemon?.name) }?`}
+                okText={`Release ${ releasePokemon?.nickname ? releasePokemon.nickname : FirstLetterToUpper(releasePokemon?.name) }`}
+                onOk={() => {
+
+                    onRelease(releasePokemon);
+
+                    setOpenModal(false)
                 }}
-            >Take back pokes</Button>
-        </GameScreen>    
+                onCancel={() => setOpenModal(false)}
+            />
+        </>
     )
 }
 
